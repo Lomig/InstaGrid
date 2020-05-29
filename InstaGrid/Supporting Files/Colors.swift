7bf4637dc1e2da8997ab83af100ed26e51e17938
Lomig Enfroy <dunwael@gmail.com>
2020-05-29 08:46:28 +0200

    Add custom colors as defined in Adobe XD file

---
+++ InstaGrid/Supporting Files/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  InstaGrid
+//
+//  Created by Lomig Enfroy on 29/05/2020.
+//  Copyright © 2020 Lomig Enfroy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    struct InstaGrid {
+        static var lightBlue:  UIColor { #colorLiteral(red: 0.7176470588, green: 0.8431372549, blue: 0.8980392157, alpha: 1) }
+        static var darkBlue:   UIColor { #colorLiteral(red: 0.06274509804, green: 0.4, blue: 0.5960784314, alpha: 1) }
+    }
+}
